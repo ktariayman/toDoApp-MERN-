e2fc2f8d578fe32dc2c3e95523e6beb15a7bf6c7
tekab aymen <tekab.ayman@gmail.com>
2022-08-08 15:52:12 +0100

    fix package.json

--- back/server.js
+++ back/server.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.listen(port, () => console.log(`Server started on port ${port}`));
