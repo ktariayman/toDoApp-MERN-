1ba820526c62c0d3a0d1572f4ef6fa850c240204
tekab aymen <tekab.ayman@gmail.com>
2022-08-09 22:45:49 +0100

    create user model , add user to TodoSchema

--- back/model/todoModel.js
+++ back/model/todoModel.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const todoSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.type.ObjectId,
+      required: true,
+      ref: "User",
+    },
     text: {
       type: String,
       required: [true, "Please add a text value"],
---
+++ back/model/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Todo", userSchema);
