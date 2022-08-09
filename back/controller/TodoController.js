7efa3c4e9960de448538e3eb23fca018b5452a97
tekab aymen <tekab.ayman@gmail.com>
2022-08-09 11:36:56 +0100

    add middleware + async-express-handler

--- back/controller/TodoController.js
+++ back/controller/TodoController.js
@@ -1,34 +1,41 @@
+const asyncHandler = require("express-async-handler");
 // @desc get Todos
 // @route GET / api / todos
 // @access Private
 
-const getTodos = (req, res) => {
-  res.status(200).json("Get Todos");
-};
+const getTodos = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get Todos" });
+});
 
 // @desc set Todo
 // @route POST / api / todos
 // @access Private
 
-const setTodo = (req, res) => {
-  res.status(200).json("set Todos");
-};
+const setTodo = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    // res.status(400).json({ message: "please add todo" });
+    throw new Error("Please add a text field");
+  }
+  console.log(req.body);
+  res.status(200).json({ message: "set Todos" });
+});
 
 // @desc update Todo
 // @route GET / api / todos
+
 // @access Private
 
-const updateTodo = (req, res) => {
-  res.status(200).json(`update todo ${req.params.id}`);
-};
+const updateTodo = asyncHandler(async (req, res) => {
+  res.status(200).json({ mesage: `update todo ${req.params.id}` });
+});
 
 // @desc delete Todo
 // @route Delete / api / todos
 // @access Private
 
-const deleteTodo = (req, res) => {
-  res.status(200).json(`delete todo ${req.params.id}`);
-};
+const deleteTodo = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `delete todo ${req.params.id}` });
+});
 module.exports = {
   getTodos,
   deleteTodo,
