c4463d47d797f0629a4ed89212a0c2ec3c5cb096
tekab aymen <tekab.ayman@gmail.com>
2022-08-09 13:43:33 +0100

    connect with mongoose + crud Todo

--- back/controller/TodoController.js
+++ back/controller/TodoController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler");
+
+const Todo = require("../model/todoModel");
 // @desc get Todos
 // @route GET / api / todos
 // @access Private
 
 const getTodos = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get Todos" });
+  const todos = await Todo.find();
+  res.status(200).json(todos);
 });
 
 // @desc set Todo
@@ -16,8 +19,11 @@ const setTodo = asyncHandler(async (req, res) => {
     // res.status(400).json({ message: "please add todo" });
     throw new Error("Please add a text field");
   }
+  const todo = await Todo.create({
+    text: req.body.text,
+  });
   console.log(req.body);
-  res.status(200).json({ message: "set Todos" });
+  res.status(200).json(todo);
 });
 
 // @desc update Todo
@@ -26,7 +32,15 @@ const setTodo = asyncHandler(async (req, res) => {
 // @access Private
 
 const updateTodo = asyncHandler(async (req, res) => {
-  res.status(200).json({ mesage: `update todo ${req.params.id}` });
+  const todo = await Todo.findById(req.params.id);
+  if (!todo) {
+    res.status(400);
+    throw new Error("todo not found");
+  }
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedTodo);
 });
 
 // @desc delete Todo
@@ -34,7 +48,13 @@ const updateTodo = asyncHandler(async (req, res) => {
 // @access Private
 
 const deleteTodo = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete todo ${req.params.id}` });
+  const todo = await Todo.findById(req.params.id);
+  if (!todo) {
+    res.status(400);
+    throw new Error("todo not found");
+  }
+  await todo.remove();
+  res.status(200).json({ id: req.params.id });
 });
 module.exports = {
   getTodos,
--- back/server.js
+++ back/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5000;
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddeware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/todos", require("./routes/TodoRoutes"));
