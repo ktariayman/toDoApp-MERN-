43191350c5bb344a225b99e5ddc0ae009828edc8
tekab aymen <tekab.ayman@gmail.com>
2022-08-09 11:51:16 +0100

    fix

--- back/server.js
+++ back/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddeware");
 const port = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddeware");
 
 const app = express();
 app.use(express.json());
